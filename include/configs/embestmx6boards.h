06a8c72b787019d868621745c97db409a4857247
Anatolij Gustschin <agust@denx.de>
2019-03-13 18:15:37 +0100

    imx: move CONFIG_VIDEO_IPUV3 to defconfigs
    Signed-off-by: Anatolij Gustschin <agust@denx.de>

--- include/configs/embestmx6boards.h
+++ include/configs/embestmx6boards.h
@@ -97,7 +97,6 @@
 #endif
 
 /* Framebuffer */
-#define CONFIG_VIDEO_IPUV3
 #define CONFIG_VIDEO_BMP_RLE8
 #define CONFIG_SPLASH_SCREEN
 #define CONFIG_SPLASH_SCREEN_ALIGN
--- include/configs/mx6sabre_common.h
+++ include/configs/mx6sabre_common.h
@@ -192,7 +192,6 @@
 #endif
 
 /* Framebuffer */
-#define CONFIG_VIDEO_IPUV3
 #define CONFIG_VIDEO_BMP_RLE8
 #define CONFIG_SPLASH_SCREEN
 #define CONFIG_SPLASH_SCREEN_ALIGN
